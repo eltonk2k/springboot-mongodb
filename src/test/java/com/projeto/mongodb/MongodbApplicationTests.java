2ac9a8d274fdc1eac597f70926f835b50ffb582e
elielton silva <elieltonpb1@gmail.com>
2021-04-06 23:15:41 -0300

    Projeto com mongodb

---
+++ src/test/java/com/projeto/mongodb/MongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
